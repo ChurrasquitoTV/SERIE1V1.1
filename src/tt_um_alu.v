dee52e7017bc9665c550c5cc1ccfd0b58f101a12
ChurrasquitoTV <bdelac@miumg.edu.gt>
2025-06-02 10:05:14 -0600

    Update tt_um_alu.v

--- src/tt_um_alu.v
+++ src/tt_um_alu.v
@@ -1,22 +1,24 @@
 module tt_um_alu (
-    input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
+    input  wire [7:0] ui_in,     // [3:0] A, [7:4] B
+    output wire [7:0] uo_out,    // Resultado de la operación
+    input  wire [7:0] uio_in,    // [2:0] SEL
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
     input  wire ena,
     input  wire clk,
     input  wire rst_n
 );
-    wire [3:0] A = ui_in[3:0];
-    wire [3:0] B = ui_in[7:4];
+    wire [3:0] A_in = ui_in[3:0];
+    wire [3:0] B_in = ui_in[7:4];
     wire [2:0] sel = uio_in[2:0];
 
+    wire [7:0] A = {4'b0000, A_in};
+    wire [7:0] B = {4'b0000, B_in};
     wire [7:0] Y;
 
     alu_8bit alu_inst (
-        .A({4'b0000, A}),
-        .B({4'b0000, B}),
+        .A(A),
+        .B(B),
         .sel(sel),
         .Y(Y)
     );
